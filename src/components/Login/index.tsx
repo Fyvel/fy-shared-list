0db1aaefe4c47e4e51273bf2bc46b2fc2b25ede5
Fyvel <fdelage8000@gmail.com>
2020-03-08 10:22:47 +1000

    review Signin page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import styles from './Login.module.scss'
-import { Button, TextField } from '@material-ui/core';
+import styles from './Login.module.scss';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import { useForm } from 'react-hook-form';
 
 type LoginProps = {
@@ -9,57 +10,50 @@ type LoginProps = {
 }
 export default function Login(props: LoginProps) {
     const { register, handleSubmit } = useForm()
-    const [displayEmailInput, setDisplayEmailInput] = useState(true)
 
-    const onSubmitLink = (data: Record<string, any>) => {
-        props.handleSigninLink(data.link)
-    }
-
-    const onSubmit = (data: Record<string, any>) => {
+    const onSubmitEmail = (data: Record<string, any>) => {
+        debugger
         props.handleLogin(data.email)
+    }
+    const onSubmitLink = (data: Record<string, any>) => {
+        debugger
+        props.handleSigninLink(data.link)
     }
 
     return (
         <>
-            {displayEmailInput ?
-                <form onSubmit={handleSubmit(onSubmit)} className={styles.Form}>
-                    <TextField
-                        inputRef={register}
-                        label="Email address"
-                        name="email"
-                        required
-                        variant="filled"
-                        type="email"
-                    />
-                    <Button className={styles.btn}
-                        type="submit"
-                        variant="contained"
-                        color="primary">
-                        SIGN IN
-                    </Button>
-                </form> :
-                <form onSubmit={handleSubmit(onSubmitLink)} className={styles.Form}>
-                    <TextField
-                        inputRef={register}
-                        label="Magic sign-in link"
-                        name="link"
-                        required
-                        variant="filled"
-                        type="text"
-                    />
-                    <Button className={styles.btn}
-                        type="submit"
-                        variant="contained"
-                        color="primary">
-                        USE THE MAGIC LINK !
-                    </Button>
-                </form>}
-            <Button className={styles.btn}
-                variant="outlined"
-                color="secondary"
-                onClick={() => setDisplayEmailInput(!displayEmailInput)}>
-                {displayEmailInput ? 'USE THE MAGIC LINK !' : 'GO TO SIGN IN VIEW'}
-            </Button>
+            <form onSubmit={handleSubmit(onSubmitEmail)} className={styles.Form}>
+                <TextField
+                    inputRef={register}
+                    label="Email address"
+                    name="email"
+                    required
+                    variant="filled"
+                    type="email" />
+                <Button className={styles.btn}
+                    type="submit"
+                    variant="contained"
+                    color="primary">
+                    BEGIN SIGN IN
+                        </Button>
+            </form>
+            <br />
+            <br />
+            <form onSubmit={handleSubmit(onSubmitLink)} className={styles.Form}>
+                <TextField
+                    inputRef={register}
+                    label="Magic sign-in link"
+                    name="link"
+                    required
+                    variant="filled"
+                    type="text" />
+                <Button className={styles.btn}
+                    type="submit"
+                    variant="contained"
+                    color="primary">
+                    COMPLETE SIGN IN
+                </Button>
+            </form>
         </>
     )
 }
